5ee2eeebcd6bf025a826f999c04d79dfa1561892
GitHub Action <action@github.com>
2023-01-07 16:57:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/willena/codereviewintelijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/willena/codereviewintelijplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.willena.codereviewintelijplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.willena.codereviewintelijplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/willena/codereviewintelijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/willena/codereviewintelijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.willena.codereviewintelijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.willena.codereviewintelijplugin.MyBundle
 
 class MyApplicationService {
 
